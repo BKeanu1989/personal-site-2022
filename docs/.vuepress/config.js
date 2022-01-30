1333d3cece843482cc53bc1fe647cd237fad78ad
Kevin Fechner <kevin@mokka-merch.com>
2022-01-30 14:00:18 +0100

    chore: progress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // site config
+  lang: 'de-DE',
+  title: 'Persönlicher Blog - Kevin Fechner',
+  description: 'This is my first VuePress site',
+
+  // theme and its config
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+        {
+            text: 'Wordpress',
+            link: '/wordpress/'
+        }
+    ]
+  },
+}
